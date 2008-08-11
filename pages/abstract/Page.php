0f9834d4535e08c7adc33f8d724609b2ecbefc20
Pierre Schmitz <pierre@archlinux.de>
2008-08-11 22:54:35 +0200

    correct doctype

--- pages/abstract/Page.php
+++ pages/abstract/Page.php
@@ -122,7 +122,8 @@ protected function makeSubMenu()
 private function sendOutput()
 	{
 	$file = '<?xml version="1.0" encoding="UTF-8" ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "xhtml11.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+   "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="de">
 	<head>
 		<title>archlinux.de :: '.$this->getValue('title').'</title>
