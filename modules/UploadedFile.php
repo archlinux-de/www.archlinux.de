46d7e3adc35f8d26a6f8c827c76a4dff132363a9
Pierre Schmitz <pierre@archlinux.de>
2010-05-28 11:53:20 +0200

    replace more occurence of LL with archlinux.de

--- modules/UploadedFile.php
+++ modules/UploadedFile.php
@@ -4,12 +4,12 @@
 
 	This file is part of archlinux.de.
 
-	LL is free software: you can redistribute it and/or modify
+	archlinux.de is free software: you can redistribute it and/or modify
 	it under the terms of the GNU Affero General Public License as published by
 	the Free Software Foundation, either version 3 of the License, or
 	(at your option) any later version.
 
-	LL is distributed in the hope that it will be useful,
+	archlinux.de is distributed in the hope that it will be useful,
 	but WITHOUT ANY WARRANTY; without even the implied warranty of
 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 	GNU Affero General Public License for more details.
