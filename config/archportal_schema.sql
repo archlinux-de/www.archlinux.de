3dd74bb682a3b188274a7033eaf3ffe5ec5b199d
Pierre Schmitz <pierre@archlinux.de>
2012-07-27 08:26:07 +0200

    Update schema

--- config/archportal_schema.sql
+++ config/archportal_schema.sql
@@ -159,6 +159,15 @@ CREATE TABLE `packages` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `pkgstats_modules` (
+  `name` varchar(255) NOT NULL,
+  `month` mediumint(6) unsigned NOT NULL,
+  `count` mediumint(8) unsigned NOT NULL,
+  PRIMARY KEY (`name`,`month`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `pkgstats_packages` (
   `pkgname` varchar(255) NOT NULL,
   `month` mediumint(6) unsigned NOT NULL,
@@ -175,6 +184,7 @@ CREATE TABLE `pkgstats_users` (
   `countryCode` char(2) DEFAULT NULL,
   `mirror` varchar(255) DEFAULT NULL,
   `packages` smallint(5) unsigned NOT NULL,
+  `modules` smallint(5) unsigned DEFAULT NULL,
   KEY `mirror` (`mirror`(20)),
   KEY `ip` (`ip`(20),`time`),
   KEY `countryCode` (`countryCode`)
