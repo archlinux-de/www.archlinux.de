8bf9fbe5cef52d136c53e0a0918cc8d7eee0587f
Pierre Schmitz <pierre@archlinux.de>
2017-08-18 18:57:35 +0200

    Migrate legacy mirror redirector to Symfony controller

--- src/AppBundle/Controller/MirrorController.php
+++ src/AppBundle/Controller/MirrorController.php
@@ -1,22 +1,21 @@
 <?php
 
-namespace archportal\pages;
+namespace AppBundle\Controller;
 
 use archportal\lib\Config;
 use archportal\lib\Input;
-use archportal\lib\Output;
 use Doctrine\DBAL\Driver\Connection;
-use PDO;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
-class GetFileFromMirror extends Output
+class MirrorController extends Controller
 {
     /** @var int */
     private $lastsync = 0;
-    /** @var string */
-    private $file = '';
     /** @var Connection */
     private $database;
     /** @var string */
@@ -29,26 +28,26 @@ class GetFileFromMirror extends Output
      */
     public function __construct(Connection $connection)
     {
-        parent::__construct();
         $this->database = $connection;
     }
 
-    public function prepare(Request $request)
+    /**
+     * @Route("/mirror/{file}")
+     * @return Response
+     */
+    public function indexAction(string $file, Request $request): Response
     {
-        $this->disallowCaching();
-        $this->setContentType('text/plain; charset=UTF-8');
-        $this->file = $request->get('file', '');
-        if (!preg_match('#^[a-zA-Z0-9\.\-\+_/:]{1,255}$#', $this->file)) {
+        $this->get('AppBundle\Service\LegacyEnvironment')->initialize();
+
+        if (!preg_match('#^[a-zA-Z0-9\.\-\+_/:]{1,255}$#', $file)) {
             throw new BadRequestHttpException('Invalid file name');
         }
-        if (strpos($this->file, '/') === 0) {
-            $this->file = substr($this->file, 1);
-        }
+
         $repositories = implode('|', array_keys(Config::get('packages', 'repositories')));
         $architectures = implode('|', $this->getAvailableArchitectures());
-        $pkgextension = '(?:'.$architectures.'|any).pkg.tar.(?:g|x)z';
-        if (preg_match('#^('.$repositories.')/os/('.$architectures.')/([^-]+.*)-[^-]+-[^-]+-'.$pkgextension.'$#',
-            $this->file, $matches)) {
+        $pkgextension = '(?:' . $architectures . '|any).pkg.tar.(?:g|x)z';
+        if (preg_match('#^(' . $repositories . ')/os/(' . $architectures . ')/([^-]+.*)-[^-]+-[^-]+-' . $pkgextension . '$#',
+            $file, $matches)) {
             $pkgdate = $this->database->prepare('
                 SELECT
                     packages.mtime
@@ -63,15 +62,15 @@ class GetFileFromMirror extends Output
                     AND repositories.name = :repository
                     AND architectures.name = :architecture
                 ');
-            $pkgdate->bindParam('pkgname', $matches[3], PDO::PARAM_STR);
-            $pkgdate->bindParam('repository', $matches[1], PDO::PARAM_STR);
-            $pkgdate->bindParam('architecture', $matches[2], PDO::PARAM_STR);
+            $pkgdate->bindParam('pkgname', $matches[3], \PDO::PARAM_STR);
+            $pkgdate->bindParam('repository', $matches[1], \PDO::PARAM_STR);
+            $pkgdate->bindParam('architecture', $matches[2], \PDO::PARAM_STR);
             $pkgdate->execute();
             if ($pkgdate->rowCount() == 0) {
                 throw new NotFoundHttpException('Package was not found');
             }
             $this->lastsync = $pkgdate->fetchColumn();
-        } elseif (preg_match('#^iso/([0-9]{4}\.[0-9]{2}\.[0-9]{2})/#', $this->file, $matches)) {
+        } elseif (preg_match('#^iso/([0-9]{4}\.[0-9]{2}\.[0-9]{2})/#', $file, $matches)) {
             $releasedate = $this->database->prepare('
                 SELECT
                     created
@@ -81,7 +80,7 @@ class GetFileFromMirror extends Output
                     version = :version
                     AND available = 1
                 ');
-            $releasedate->bindParam('version', $matches[1], PDO::PARAM_STR);
+            $releasedate->bindParam('version', $matches[1], \PDO::PARAM_STR);
             $releasedate->execute();
             if ($releasedate->rowCount() == 0) {
                 throw new NotFoundHttpException('ISO image was not found');
@@ -92,12 +91,9 @@ class GetFileFromMirror extends Output
         }
 
         $this->clientId = crc32($request->getClientIp());
-        $this->targetUrl = $this->getMirror($this->lastsync).$this->file;
-    }
+        $this->targetUrl = $this->getMirror($this->lastsync) . $file;
 
-    public function printPage()
-    {
-        $this->redirectToUrl($this->targetUrl);
+        return $this->redirect($this->targetUrl);
     }
 
     /**
@@ -137,9 +133,9 @@ class GetFileFromMirror extends Output
                 AND protocol IN ("http", "htttps")
             ORDER BY RAND(:clientId) LIMIT 1
             ');
-        $stm->bindParam('lastsync', $lastsync, PDO::PARAM_INT);
-        $stm->bindParam('countryCode', $countryCode, PDO::PARAM_STR);
-        $stm->bindParam('clientId', $this->clientId, PDO::PARAM_INT);
+        $stm->bindParam('lastsync', $lastsync, \PDO::PARAM_INT);
+        $stm->bindParam('countryCode', $countryCode, \PDO::PARAM_STR);
+        $stm->bindParam('clientId', $this->clientId, \PDO::PARAM_INT);
         $stm->execute();
         if ($stm->rowCount() == 0) {
             // Let's see if any mirror is recent enough
@@ -153,8 +149,8 @@ class GetFileFromMirror extends Output
                     AND protocol IN ("http", "htttps")
                 ORDER BY RAND(:clientId) LIMIT 1
                 ');
-            $stm->bindParam('lastsync', $lastsync, PDO::PARAM_INT);
-            $stm->bindParam('clientId', $this->clientId, PDO::PARAM_INT);
+            $stm->bindParam('lastsync', $lastsync, \PDO::PARAM_INT);
+            $stm->bindParam('clientId', $this->clientId, \PDO::PARAM_INT);
             $stm->execute();
             if ($stm->rowCount() == 0) {
                 throw new NotFoundHttpException('File was not found');
