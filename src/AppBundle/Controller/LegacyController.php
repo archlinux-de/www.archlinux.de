74d0d3ddac7f44d44ebdde8ce3906bd62e1088c4
Pierre Schmitz <pierre@archlinux.de>
2017-08-19 14:35:04 +0200

    Redirect old URLs to the new ones

--- src/AppBundle/Controller/LegacyController.php
+++ src/AppBundle/Controller/LegacyController.php
@@ -2,37 +2,74 @@
 
 namespace AppBundle\Controller;
 
-use archportal\lib\Page;
-use archportal\lib\Routing;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\RouterInterface;
 
 class LegacyController extends Controller
 {
+    /** @var RouterInterface */
+    private $router;
+    /** @var array */
+    private $internalPages = array(
+        'GetFileFromMirror' => 'app_mirror_index',
+        'GetOpenSearch' => 'app_getopensearch_index',
+        'GetRecentNews' => 'app_recentnews_index',
+        'GetRecentPackages' => 'app_recentpackages_index',
+        'MirrorStatus' => 'app_mirrorstatus_index',
+        'PackageDetails' => 'app_packagedetails_index',
+        'Packagers' => 'app_packagers_index',
+        'Packages' => 'app_packages_index',
+        'PackagesSuggest' => 'app_packagessuggest_suggest',
+        'Start' => 'app_start_index',
+        'FunStatistics' => 'app_statistics_funstatistics_fun',
+        'ModuleStatistics' => 'app_statistics_modulestatistics_module',
+        'PackageStatistics' => 'app_statistics_packagestatistics_package',
+        'RepositoryStatistics' => 'app_statistics_repositorystatistics_repository',
+        'Statistics' => 'app_statistics_statistics_index',
+        'UserStatistics' => 'app_statistics_userstatistics_user'
+    );
+    /** @var array */
+    private $externalPages = array(
+        'ArchitectureDifferences' => 'https://www.archlinux.org/packages/differences/',
+        'MirrorProblems' => 'https://www.archlinux.org/mirrors/status/#outofsync',
+        'MirrorStatusJSON' => 'https://www.archlinux.org/mirrors/status/json/'
+    );
+
     /**
-     * @Route("/", condition="request.query.has('page')")
+     * @param RouterInterface $router
+     */
+    public function __construct(RouterInterface $router)
+    {
+        $this->router = $router;
+    }
+
+    /**
+     * @Route("/", condition="request.query.has('page')", methods={"GET"})
      * @param Request $request
      * @return Response
      */
     public function pageAction(Request $request): Response
     {
-        $this->get('AppBundle\Service\LegacyEnvironment')->initialize($request);
+        $page = $request->get('page');
 
-        $page = Routing::getPageClass($request->get('page', 'Start'));
-        /** @var Page $thisPage */
-        $thisPage = $this->get($page);
+        if (isset($this->internalPages[$page])) {
+            $parameters = array_diff_key($request->query->all(), ['page' => '']);
+            return $this->redirectToRoute($this->internalPages[$page], $parameters, Response::HTTP_MOVED_PERMANENTLY);
+        } elseif (isset($this->externalPages[$page])) {
+            return $this->redirect($this->externalPages[$page], Response::HTTP_MOVED_PERMANENTLY);
+        }
+        throw $this->createNotFoundException();
+    }
 
-        $thisPage->prepare($request);
-        ob_start();
-        $thisPage->printPage();
-        $pageContent = ob_get_clean();
-
-        return new Response(
-            $pageContent,
-            $thisPage->getStatus(),
-            array_merge(['Content-Type' => $thisPage->getContentType()], $thisPage->getHeaders())
-        );
+    /**
+     * @Route("/", condition="request.query.get('page') == 'PostPackageList'", methods={"POST"})
+     * @return Response
+     */
+    public function postAction(): Response
+    {
+        return $this->forward('AppBundle:Statistics\PostPackageList:post');
     }
 }
