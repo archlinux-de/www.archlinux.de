d7d90dc28c30779555fa2c17c9830759fc862ad3
Pierre Schmitz <pierre@archlinux.de>
2017-08-19 18:42:28 +0200

    Test legacy pages are redirected properly

--- tests/AppBundle/Controller/LegacyControllerTest.php
+++ tests/AppBundle/Controller/LegacyControllerTest.php
@@ -6,13 +6,70 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class LegacyControllerTest extends WebTestCase
 {
-    public function testIndex()
+    /**
+     * @param string $legacyPage
+     * @param array $parameters
+     * @dataProvider provideLegacyPages
+     */
+    public function testLegacyPagesAreRedirected(string $legacyPage, array $parameters = [])
     {
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/');
+        $client->request('GET', '/', array_merge(['page' => $legacyPage], $parameters));
 
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('Willkommen bei Arch Linux', $crawler->filter('#intro h2')->text());
+        $this->assertTrue($client->getResponse()->isRedirection());
+        foreach ($parameters as $parameter) {
+            $this->assertContains($parameter, $client->getResponse()->headers->get('Location'));
+        }
+    }
+
+    /**
+     * @return array
+     */
+    public function provideLegacyPages(): array
+    {
+        return [
+            ['GetFileFromMirror', ['file' => 'foo']],
+            ['GetOpenSearch'],
+            ['GetRecentNews'],
+            ['GetRecentPackages'],
+            ['MirrorStatus'],
+            ['PackageDetails', ['repo' => 'core', 'arch' => 'x86_64', 'pkgname' => 'foo']],
+            ['Packagers'],
+            ['Packages'],
+            ['PackagesSuggest'],
+            ['Start'],
+            ['FunStatistics'],
+            ['ModuleStatistics'],
+            ['PackageStatistics'],
+            ['RepositoryStatistics'],
+            ['Statistics'],
+            ['UserStatistics'],
+            ['ArchitectureDifferences'],
+            ['MirrorProblems'],
+            ['MirrorStatusJSON']
+        ];
+    }
+
+    public function testPostPackageListStillAcceptsInput()
+    {
+        $client = static::createClient();
+
+        $client->request(
+            'POST',
+            '/?page=PostPackageList',
+            ['pkgstatsver' => '2.3', 'arch' => 'x86_64', 'packages' => 'pkgstats', 'modules' => 'snd']
+        );
+
+        $this->assertTrue($client->getResponse()->isSuccessful());
+    }
+
+    public function testUnknownPageWillFail()
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/', ['page' => 'UnknownPage']);
+
+        $this->assertTrue($client->getResponse()->isNotFound());
     }
 }
