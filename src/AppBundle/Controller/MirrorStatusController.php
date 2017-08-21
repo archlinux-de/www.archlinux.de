d1ca469d56946aa15b38f0fcd1c96e3541ec896c
Pierre Schmitz <pierre@archlinux.de>
2017-08-21 12:16:10 +0200

    Use DataTables on mirror status page

--- src/AppBundle/Controller/MirrorStatusController.php
+++ src/AppBundle/Controller/MirrorStatusController.php
@@ -3,32 +3,12 @@
 namespace AppBundle\Controller;
 
 use Doctrine\DBAL\Driver\Connection;
-use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
 class MirrorStatusController extends Controller
 {
-    /** @var string */
-    private $orderby = 'lastsync';
-    /** @var string */
-    private $sort = 'desc';
-    /** @var int */
-    private $range = 604800; // 1 week
-    /** @var array */
-    private $orders = array(
-        'url',
-        'country',
-        'lastsync',
-        'delay',
-        'durationAvg',
-    );
-    /** @var array */
-    private $sorts = array(
-        'asc',
-        'desc',
-    );
     /** @var Connection */
     private $database;
 
@@ -42,38 +22,25 @@ class MirrorStatusController extends Controller
 
     /**
      * @Route("/mirrors", methods={"GET"})
-     * @param Request $request
      * @return Response
      */
-    public function indexAction(Request $request): Response
+    public function indexAction(): Response
     {
-        if (in_array($request->get('orderby'), $this->orders)) {
-            $this->orderby = $request->get('orderby');
-        }
-        if (in_array($request->get('sort'), $this->sorts)) {
-            $this->sort = $request->get('sort');
-        }
-
         $mirrors = $this->database->query('
         SELECT
             mirrors.url,
             countries.name AS country,
-            mirrors.lastsync,
+            mirrors.durationAvg,
             mirrors.delay,
-            mirrors.durationAvg
+            mirrors.lastsync
         FROM
             mirrors
             JOIN countries
             ON mirrors.countryCode = countries.code
-        WHERE
-            mirrors.lastsync >= ' . (time() - $this->range) . '
-        ORDER BY
-            ' . $this->orderby . ' ' . $this->sort . '
-        ');
+        ')->fetchAll(\PDO::FETCH_ASSOC);
 
         return $this->render('mirrors/index.html.twig', [
-            'mirrors' => $mirrors,
-            'sort' => $this->sort == 'desc' ? 'asc' : 'desc'
-        ]);
+            'mirrors' => $mirrors
+        ])->setSharedMaxAge(600);
     }
 }
