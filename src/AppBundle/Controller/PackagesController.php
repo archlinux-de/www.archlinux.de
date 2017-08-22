5a4189e6cb8f140ce18afcf42f46e3ef543b60ac
Pierre Schmitz <pierre@archlinux.de>
2017-08-22 15:41:23 +0200

    Implement application caching for the packages endpoint

--- src/AppBundle/Controller/PackagesController.php
+++ src/AppBundle/Controller/PackagesController.php
@@ -3,14 +3,27 @@
 namespace AppBundle\Controller;
 
 use Doctrine\DBAL\Query\QueryBuilder;
+use Psr\Cache\CacheItemPoolInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use AppBundle\Request\Datatables\Request as DatatablesRequest;
+use AppBundle\Response\Datatables\Response as DatatablesResponse;
 
 class PackagesController extends Controller
 {
+    /** @var CacheItemPoolInterface */
+    private $cache;
+
+    /**
+     * @param CacheItemPoolInterface $cache
+     */
+    public function __construct(CacheItemPoolInterface $cache)
+    {
+        $this->cache = $cache;
+    }
+
     /**
      * @Route("/packages", methods={"GET"})
      * @param Request $request
@@ -32,6 +45,50 @@ class PackagesController extends Controller
      * @return Response
      */
     public function datatablesAction(DatatablesRequest $request): Response
+    {
+        $cachedResponse = $this->cache->getItem($request->getId());
+        if ($cachedResponse->isHit()) {
+            /** @var DatatablesResponse $response */
+            $response = $cachedResponse->get();
+        } else {
+            $response = $this->queryDatabase($request);
+            $cachedResponse->expiresAt(new \DateTime('1 hour'));
+            $cachedResponse->set($response);
+            // Only store the first draw (initial state of the page)
+            if ($request->getDraw() == 1) {
+                $this->cache->save($cachedResponse);
+            }
+        }
+
+        $cachedPackageCount = $this->cache->getItem('packages.count');
+        if ($cachedPackageCount->isHit()) {
+            /** @var int $packageCount */
+            $packageCount = $cachedPackageCount->get();
+        } else {
+            $packageCount = $this->getDoctrine()->getConnection()->createQueryBuilder()
+                ->select('COUNT(*)')->from('packages')->execute()->fetchColumn();
+            $cachedPackageCount->expiresAt(new \DateTime('24 hour'));
+            $cachedPackageCount->set($packageCount);
+            $this->cache->save($cachedPackageCount);
+        }
+
+        $response->setRecordsTotal($packageCount);
+        $response->setDraw($request->getDraw());
+
+        return $this->json(
+            $response,
+            Response::HTTP_OK,
+            [
+                'X-Cache-App' => $cachedResponse->isHit() ? 'HIT' : 'MISS'
+            ]
+        );
+    }
+
+    /**
+     * @param DatatablesRequest $request
+     * @return DatatablesResponse
+     */
+    private function queryDatabase(DatatablesRequest $request): DatatablesResponse
     {
         $compareableColumns = [
             'repository' => 'repositories.name',
@@ -118,15 +175,12 @@ class PackagesController extends Controller
 
         $packagesFiltered = $connection->createQueryBuilder()
             ->select('FOUND_ROWS()')->execute()->fetchColumn();
-        $totalPackages = $connection->createQueryBuilder()
-            ->select('COUNT(*)')->from('packages')->execute()->fetchColumn();
 
-        return $this->json([
-            'draw' => $request->getDraw(),
-            'recordsTotal' => (int)$totalPackages,
-            'recordsFiltered' => (int)$packagesFiltered,
-            'data' => $packages
-        ])->setSharedMaxAge(600);
+        $response = new DatatablesResponse();
+        $response->setRecordsFiltered($packagesFiltered);
+        $response->setData($packages);
+
+        return $response;
     }
 
     /**
