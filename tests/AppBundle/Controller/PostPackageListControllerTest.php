925d201d490f9dc26bfb36d6ee9371e5ac051d52
Pierre Schmitz <pierre@archlinux.de>
2017-08-26 02:13:56 +0200

    Avoid post package list rate limit during tests

--- tests/AppBundle/Controller/PostPackageListControllerTest.php
+++ tests/AppBundle/Controller/PostPackageListControllerTest.php
@@ -6,6 +6,16 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class PostPackageListControllerTest extends WebTestCase
 {
+    public function setUp()
+    {
+        //TODO: Remove entries from previous runs to avoid the rate limit.
+        // Should be obsolete once we are able to setup a proper test environment.
+        $kernel = static::bootKernel();
+        $kernel->getContainer()
+            ->get('database_connection')
+            ->exec('DELETE FROM pkgstats_users WHERE ip IN (SHA1("127.0.0.1"), SHA1("::1"))');
+    }
+
     public function testPostPackageListIsSuccessful()
     {
         $client = static::createClient();
