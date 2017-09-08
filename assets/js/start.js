23f49cc619bc09b17564d846acc26b5639b0d6bd
Pierre Schmitz <pierre@archlinux.de>
2017-09-08 21:04:39 +0200

    Use JQuery to fetch data

--- assets/js/start.js
+++ assets/js/start.js
@@ -4,25 +4,12 @@ import AutoComplete from 'js-autocomplete'
 $(document).ready(function () {
   const searchInput = document.getElementById('searchfield')
   const suggest = searchInput.dataset.suggest
-  const emptyData = []
   AutoComplete({
     selector: searchInput,
     delay: 100,
     minChars: 1,
-    source: function (term, callback) {
-      window.fetch(suggest + '?term=' + encodeURI(term))
-        .then(function (response) {
-          if (!response.ok) {
-            throw new Error()
-          }
-          return response.json()
-        })
-        .then(function (data) {
-          return callback(data)
-        })
-        .catch(function () {
-          return callback(emptyData)
-        })
+    source: function (term, response) {
+      $.getJSON(suggest, {term: term}, function (data) { response(data) })
     }
   })
 })
