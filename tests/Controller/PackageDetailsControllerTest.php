afd2de14f33b365f6019eb02ddeb97009e3e1179
Pierre Schmitz <pierre@archlinux.de>
2017-12-06 19:31:20 +0100

    Add controller test

---
+++ tests/Controller/PackageDetailsControllerTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class PackageDetailsControllerTest extends WebTestCase
+{
+    public function testUnknownPackageReturnsCorrectHttpStatus()
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/packages/core/x86_64/not-found');
+
+        $this->assertTrue($client->getResponse()->isNotFound());
+    }
+}
