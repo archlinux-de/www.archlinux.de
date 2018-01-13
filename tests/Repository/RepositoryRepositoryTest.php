803582019fdc37a1c6cf9a4ad5160f1e4e59a431
Pierre Schmitz <pierre@archlinux.de>
2018-01-13 21:21:05 +0100

    Add repository repository test

---
+++ tests/Repository/RepositoryRepositoryTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Tests\Repository;
+
+use App\Entity\Packages\Architecture;
+use App\Entity\Packages\Repository;
+use App\Repository\RepositoryRepository;
+use App\Tests\Util\DatabaseTestCase;
+
+class RepositoryRepositoryTest extends DatabaseTestCase
+{
+    public function testFindByNameAndArchitecture()
+    {
+        $repository = new Repository('core', Architecture::X86_64);
+
+        $entityManager = $this->getEntityManager();
+        $entityManager->persist($repository);
+        $entityManager->flush();
+        $entityManager->clear();
+
+        /** @var RepositoryRepository $repositoryRepository */
+        $repositoryRepository = $this->getRepository(Repository::class);
+        $databaseRepository = $repositoryRepository->findByNameAndArchitecture('core', Architecture::X86_64);
+        $this->assertEquals($repository->getId(), $databaseRepository->getId());
+    }
+}
