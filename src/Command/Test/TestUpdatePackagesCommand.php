0c15d506c5df927ebc5cbe63f3f2a62d6865798e
Pierre Schmitz <pierre@archlinux.de>
2018-03-03 16:13:01 +0100

    Add test command to catch update issues

---
+++ src/Command/Test/TestUpdatePackagesCommand.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Command\Test;
+
+use GuzzleHttp\Client;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Process\Process;
+
+class TestUpdatePackagesCommand extends ContainerAwareCommand
+{
+    /** @var Client */
+    private $guzzleClient;
+
+    /**
+     * @param Client $guzzleClient
+     */
+    public function __construct(Client $guzzleClient)
+    {
+        parent::__construct();
+        $this->guzzleClient = $guzzleClient;
+    }
+
+    protected function configure()
+    {
+        $this->setName('app:test:update-packages');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $clearCache = new Process(['bin/console', 'cache:clear']);
+        $clearCache->mustRun();
+
+        $clearAppCache = new Process(['bin/console', 'cache:pool:clear', 'cache.app']);
+        $clearAppCache->mustRun();
+
+        $resetDatabase = new Process(['bin/console', 'app:reset:database', '--packages']);
+        $resetDatabase->mustRun();
+
+        $updateRepositories = new Process(['bin/console', 'app:update:repositories']);
+        $updateRepositories->mustRun();
+
+        foreach (range(2017, 2018) as $year) {
+            foreach (range(1, 12) as $month) {
+                foreach (range(1, 31) as $day) {
+                    $mirrorUrl = sprintf('https://archive.archlinux.org/repos/%d/%02d/%02d/', $year, $month, $day);
+                    if ($this->checkMirrorUrl($mirrorUrl)) {
+                        $output->writeln(sprintf('Updating to %d-%02d-%02d', $year, $month, $day));
+                        $updatePackages = new Process(
+                            ['bin/console', 'app:update:packages'],
+                            null,
+                            ['PACKAGES_MIRROR' => $mirrorUrl]
+                        );
+                        $updatePackages->setTimeout(120);
+                        $updatePackages->mustRun();
+                        if (!$updatePackages->isSuccessful()) {
+                            $output->writeln($updatePackages->getOutput());
+                            return 1;
+                        } else {
+                            $output->writeln($updatePackages->getOutput());
+                        }
+
+                        $validatePackages = new Process(
+                            ['bin/console', 'app:validate:packages'],
+                            null,
+                            ['PACKAGES_MIRROR' => $mirrorUrl]
+                        );
+                        $validatePackages->mustRun();
+                        if (!$validatePackages->isSuccessful()) {
+                            $output->writeln($validatePackages->getOutput());
+                            return 1;
+                        }
+                    }
+                }
+            }
+        }
+
+        return 0;
+    }
+
+    /**
+     * @param string $url
+     * @return bool
+     */
+    private function checkMirrorUrl(string $url): bool
+    {
+        try {
+            $content = $this->guzzleClient->request(
+                'GET',
+                $url . 'lastupdate'
+            )->getBody()->getContents();
+            return $content > 1;
+        } catch (\RuntimeException $e) {
+            return false;
+        }
+    }
+}
---
+++ src/Command/Validate/ValidatePackagesCommand.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace App\Command\Validate;
+
+use App\ArchLinux\Package as DatabasePackage;
+use App\ArchLinux\PackageDatabase;
+use App\ArchLinux\PackageDatabaseDownloader;
+use App\ArchLinux\PackageDatabaseMirror;
+use App\ArchLinux\PackageDatabaseReader;
+use App\Entity\Packages\Package;
+use App\Entity\Packages\Repository;
+use App\Repository\PackageRepository;
+use App\Repository\RepositoryRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Command\LockableTrait;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ValidatePackagesCommand extends ContainerAwareCommand
+{
+    use LockableTrait;
+
+    /** @var PackageDatabaseDownloader */
+    private $packageDatabaseDownloader;
+
+    /** @var EntityManagerInterface */
+    private $entityManager;
+
+    /** @var PackageDatabaseMirror */
+    private $packageDatabaseMirror;
+
+    /** @var RepositoryRepository */
+    private $repositoryRepository;
+
+    /** @var PackageRepository */
+    private $packageRepository;
+
+    /**
+     * @param PackageDatabaseDownloader $packageDatabaseDownloader
+     * @param PackageDatabaseMirror $packageDatabaseMirror
+     * @param RepositoryRepository $repositoryRepository
+     * @param PackageRepository $packageRepository
+     */
+    public function __construct(
+        PackageDatabaseDownloader $packageDatabaseDownloader,
+        PackageDatabaseMirror $packageDatabaseMirror,
+        RepositoryRepository $repositoryRepository,
+        PackageRepository $packageRepository
+    ) {
+        parent::__construct();
+        $this->packageDatabaseDownloader = $packageDatabaseDownloader;
+        $this->packageDatabaseMirror = $packageDatabaseMirror;
+        $this->repositoryRepository = $repositoryRepository;
+        $this->packageRepository = $packageRepository;
+    }
+
+    protected function configure()
+    {
+        $this->setName('app:validate:packages');
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return int|null
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->lock('cron.lock', true);
+        ini_set('memory_limit', '-1');
+
+        $result = 0;
+
+        /** @var DatabasePackage $package */
+
+        foreach ($this->findMissingPackages() as $repo => $package) {
+            $output->writeln(sprintf('Missing package: %s [%s]', $package, $repo));
+            $result = 1;
+        }
+
+        foreach ($this->findObsoletePackages() as $repo => $package) {
+            $output->writeln(sprintf('Obsolete package: %s [%s]', $package, $repo));
+            $result = 1;
+        }
+
+        $this->release();
+
+        return $result;
+    }
+
+    private function findMissingPackages(): iterable
+    {
+        /** @var Repository $repo */
+        foreach ($this->repositoryRepository->findAll() as $repo) {
+            /** @var DatabasePackage $package */
+            foreach ($this->downloadPackagesForRepository($repo) as $package) {
+                if (is_null($this->packageRepository->findByRepositoryAndName($repo, $package->getName()))) {
+                    yield $repo->getName() => $package->getName();
+                }
+            }
+        }
+    }
+
+    /**
+     * @param Repository $repository
+     * @return iterable
+     */
+    private function downloadPackagesForRepository(Repository $repository): iterable
+    {
+        $packageDatabaseFile = $this->packageDatabaseDownloader->download(
+            $this->packageDatabaseMirror->getMirrorUrl(),
+            $repository->getName(),
+            $repository->getArchitecture()
+        );
+        yield from new PackageDatabase(new PackageDatabaseReader($packageDatabaseFile));
+    }
+
+    /**
+     * @return iterable
+     */
+    private function findObsoletePackages(): iterable
+    {
+        /** @var Repository $repo */
+        foreach ($this->repositoryRepository->findAll() as $repo) {
+            $packageNames = [];
+            /** @var DatabasePackage $package */
+            foreach ($this->downloadPackagesForRepository($repo) as $package) {
+                $packageNames[] = $package->getName();
+            }
+
+            $repoPackages = $this->packageRepository->findByRepositoryOlderThan($repo, new \DateTime());
+            /** @var Package $repoPackage */
+            foreach ($repoPackages as $repoPackage) {
+                if (!in_array($repoPackage->getName(), $packageNames)) {
+                    yield $repo->getName() => $repoPackage->getName();
+                }
+            }
+        }
+    }
+}
