781b206babf29a7092257b8e42a472161f9260c4
Pierre Schmitz <pierre@archlinux.de>
2018-12-30 08:56:57 +0100

    Add migration for news item slugs

---
+++ src/Migrations/Version20181230070059.php
@@ -0,0 +1,78 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use App\Entity\NewsItem;
+use App\Service\NewsItemSlugger;
+use App\Service\Slugger;
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20181230070059 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema): void
+    {
+        $this->abortIf(
+            $this->connection->getDatabasePlatform()->getName() !== 'mysql',
+            'Migration can only be executed safely on \'mysql\'.'
+        );
+
+        $this->addSql('ALTER TABLE news_item ADD slug VARCHAR(191) NOT NULL AFTER id');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function postUp(Schema $schema)
+    {
+        if ($this->hasColumn('news_item', 'slug')) {
+            $this->write('foo');
+            $newsItemSlugger = new NewsItemSlugger(new Slugger());
+            foreach ($this->connection->fetchAll('SELECT id, title FROM news_item') as $row) {
+                $newsItem = (new NewsItem($row['id']))->setTitle($row['title']);
+                $this->connection->update(
+                    'news_item',
+                    ['slug' => $newsItemSlugger->slugify($newsItem)],
+                    ['id' => $newsItem->getId()]
+                );
+            }
+        } else {
+            $this->warnIf(
+                true,
+                'Column slug on table news_item was not found. Data migration was skipped!'
+            );
+        }
+    }
+
+    /**
+     * @param string $table
+     * @param string $columnName
+     * @return bool
+     */
+    private function hasColumn(string $table, string $columnName): bool
+    {
+
+        foreach ($this->connection->getSchemaManager()->listTableColumns($table) as $column) {
+            if ($column->getName() == $columnName) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema): void
+    {
+        $this->abortIf(
+            $this->connection->getDatabasePlatform()->getName() !== 'mysql',
+            'Migration can only be executed safely on \'mysql\'.'
+        );
+
+        $this->addSql('ALTER TABLE news_item DROP slug');
+    }
+}
