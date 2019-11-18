ffcdf27e65b3e2686b7f35aa77c132e53fbe7bdf
Pierre Schmitz <pierre@archlinux.de>
2019-11-18 20:29:24 +0100

    Fix column names

---
+++ src/Migrations/Version20191118191929.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20191118191929 extends AbstractMigration
+{
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf(
+            $this->connection->getDatabasePlatform()->getName() !== 'mysql',
+            'Migration can only be executed safely on \'mysql\'.'
+        );
+
+        // phpcs:disable
+        $this->addSql('ALTER TABLE releng_release CHANGE md5sum md5_sum VARCHAR(32) DEFAULT NULL, CHANGE sha1sum sha1_sum VARCHAR(40) DEFAULT NULL');
+        // phpcs:enable
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf(
+            $this->connection->getDatabasePlatform()->getName() !== 'mysql',
+            'Migration can only be executed safely on \'mysql\'.'
+        );
+
+        // phpcs:disable
+        $this->addSql('ALTER TABLE releng_release CHANGE md5_sum md5sum VARCHAR(32) CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE sha1_sum sha1sum VARCHAR(40) CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_unicode_ci`');
+        // phpcs:enable
+    }
+}
