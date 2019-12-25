7cfb4b2b8960263ca39aa44dc6d0e221964e2e8e
Pierre Schmitz <pierre@archlinux.de>
2019-12-25 13:27:20 +0100

    Complete Twig Extension test

--- tests/Twig/SluggerExtensionTest.php
+++ tests/Twig/SluggerExtensionTest.php
@@ -10,6 +10,23 @@ use Symfony\Component\String\UnicodeString;
 
 class SluggerExtensionTest extends TestCase
 {
+    public function testGetFilters(): void
+    {
+        /** @var SluggerInterface|MockObject $slugger */
+        $slugger = $this->createMock(SluggerInterface::class);
+        $slugger
+            ->expects($this->once())
+            ->method('slug')
+            ->with('foo')
+            ->willReturn(new UnicodeString('bar'));
+
+        $sluggerExtension = new SluggerExtension($slugger);
+
+        $this->assertEquals('slug', $sluggerExtension->getFilters()[0]->getName());
+        $this->assertIsCallable($sluggerExtension->getFilters()[0]->getCallable());
+        $this->assertEquals('bar', $sluggerExtension->getFilters()[0]->getCallable()('foo'));
+    }
+
     public function testSlug(): void
     {
         /** @var SluggerInterface|MockObject $slugger */
