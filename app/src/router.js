9624a9d2f9829da8a86f60578ab3e6c91dc69430
Pierre Schmitz <pierre@archlinux.de>
2021-12-31 13:54:12 +0100

    Reduce initial bundle size

--- app/src/router.js
+++ app/src/router.js
@@ -1,33 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Download from './views/Download'
-import Impressum from './views/Impressum'
-import Mirrors from './views/Mirrors'
-import News from './views/News'
-import NewsItem from './views/NewsItem'
-import Package from './views/Package'
-import Packages from './views/Packages'
-import PrivacyPolicy from './views/PrivacyPolicy'
-import Release from './views/Release'
-import Releases from './views/Releases'
-import Start from './views/Start'
 import NotFound from './views/NotFound'
 
 export default createRouter({
   history: createWebHistory(),
   linkActiveClass: 'active',
   routes: [
-    { path: '/download', name: 'download', component: Download },
-    { path: '/impressum', name: 'impressum', component: Impressum },
-    { path: '/mirrors', name: 'mirrors', component: Mirrors },
-    { path: '/news', name: 'news', component: News },
-    { path: '/news/:id-:slug', name: 'news-item', component: NewsItem },
-    { path: '/news/:id', name: 'news-item-permalink', component: NewsItem },
-    { path: '/packages/:repository/:architecture/:name', name: 'package', component: Package },
-    { path: '/packages', name: 'packages', component: Packages },
-    { path: '/privacy-policy', name: 'privacy-policy', component: PrivacyPolicy },
-    { path: '/releases/:version', name: 'release', component: Release },
-    { path: '/releases', name: 'releases', component: Releases },
-    { path: '/', name: 'start', component: Start },
+    { path: '/download', name: 'download', component: () => import(/* webpackChunkName: "releases" */ './views/Download') },
+    { path: '/impressum', name: 'impressum', component: () => import(/* webpackChunkName: "legal" */ './views/Impressum') },
+    { path: '/mirrors', name: 'mirrors', component: () => import(/* webpackChunkName: "mirrors" */ './views/Mirrors') },
+    { path: '/news', name: 'news', component: () => import(/* webpackChunkName: "news" */ './views/News') },
+    { path: '/news/:id-:slug', name: 'news-item', component: () => import(/* webpackChunkName: "news" */ './views/NewsItem') },
+    { path: '/news/:id', name: 'news-item-permalink', component: () => import(/* webpackChunkName: "news" */ './views/NewsItem') },
+    { path: '/packages/:repository/:architecture/:name', name: 'package', component: () => import(/* webpackChunkName: "packages" */ './views/Package') },
+    { path: '/packages', name: 'packages', component: () => import(/* webpackChunkName: "packages" */ './views/Packages') },
+    { path: '/privacy-policy', name: 'privacy-policy', component: () => import(/* webpackChunkName: "legal" */ './views/PrivacyPolicy') },
+    { path: '/releases/:version', name: 'release', component: () => import(/* webpackChunkName: "releases" */ './views/Release') },
+    { path: '/releases', name: 'releases', component: () => import(/* webpackChunkName: "releases" */ './views/Releases') },
+    { path: '/', name: 'start', component: () => import(/* webpackChunkName: "start" */ './views/Start') },
     { path: '/:pathMatch(.*)*', component: NotFound }
   ],
   scrollBehavior (to, from, savedPosition) {
