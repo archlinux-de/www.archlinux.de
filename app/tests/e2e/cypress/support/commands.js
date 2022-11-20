1b234cf4c6cac7b5abda404ed1e79376e04512de
Pierre Schmitz <pierre@archlinux.de>
2022-11-20 14:07:21 +0100

    Wait for initial content to be rendered

--- app/tests/e2e/cypress/support/commands.js
+++ app/tests/e2e/cypress/support/commands.js
@@ -1,6 +1,4 @@
 Cypress.Commands.overwrite('visit', (originalFn, url, options) => {
   originalFn(url, options)
-  // @FIXME: Find a way to detect if data fetching is finished
-  // eslint-disable-next-line cypress/no-unnecessary-waiting
-  return cy.wait(500)
+  cy.get('#content').should('be.visible')
 })
